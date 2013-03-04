8a5c2f6fc4ce3377ac186162bfbe818f6f4f8451
Konstantin Läufer <laufer@cs.luc.edu>
2013-03-04 13:11:17 -0700

    added new unbounded counter abstraction

---
+++ src/main/scala/edu/luc/etl/cs313/android/scala/clickcounter/model/Counter.scala
@@ -0,0 +1,25 @@
+package edu.luc.etl.cs313.android.scala.clickcounter
+package model
+
+/**
+ * An immutable counter abstraction.
+ *
+ * @author laufer
+ */
+trait Counter {
+
+  /**
+   * Increments the counter value.
+   */
+  def increment(value: Int): Option[Int]
+
+  /**
+   * Decrements the counter value.
+   */
+  def decrement(value: Int): Option[Int]
+
+  /**
+   * Resets the counter value.
+   */
+  def reset(value: Int): Option[Int]
+}
