88614b2c6b1030f3b26360e6e353944d6548ff39
Konstantin Läufer <laufer@cs.luc.edu>
2014-11-08 13:40:33 -0600

    moved prereq install into separate script

---
+++ install-prereqs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Installs the required prerequisites for an sbt-based Android build.
+# Designed for use with Travis-CI.
+
+# install required Ubuntu packages
+
+sudo apt-get update -qq
+sudo apt-get install -qq lib32stdc++6 lib32z1
+
+# manually install Android SDK components
+
+export ANDROID_SDK_VERSION=23.0.2
+export ANDROID_HOME=$PWD/android-sdk-linux
+export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
+
+wget http://dl.google.com/android/android-sdk_r${ANDROID_SDK_VERSION}-linux.tgz
+tar -zxf android-sdk_r${ANDROID_SDK_VERSION}-linux.tgz
+echo yes | android update sdk --filter tools,platform-tools,build-tools-21.1.1,android-19,extra-android-support,extra-android-m2repository,extra-google-m2repository --no-ui --force --no-https --all > /dev/null
+
+# create local.properties for Android build system to find SDK
+
+echo sdk.dir=${ANDROID_HOME} > local.properties
