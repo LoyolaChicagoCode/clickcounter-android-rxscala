5b19a1586c82a1ac9cf5e0ebecb2f746bcca8ba3
Konstantin Läufer <laufer@cs.luc.edu>
2014-11-21 08:02:37 -0600

    fixed ANDROID_HOME in Travis env

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -4,12 +4,6 @@
 # Designed for use with Travis-CI, but can also be used locally
 # (assuming JDK and sbt are already installed).
 
-echo reviewing default Unix environment variables
-
-env
-
-exit 1
-
 # install required Ubuntu packages
 
 sudo apt-get update -qq
@@ -18,13 +12,8 @@ sudo apt-get install -qq lib32stdc++6 lib32z1
 # manually install Android SDK components
 
 export ANDROID_SDK_VERSION=23.0.2
-#export ANDROID_HOME=$PWD/android-sdk-linux
 export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
 
 wget http://dl.google.com/android/android-sdk_r${ANDROID_SDK_VERSION}-linux.tgz
 tar -zxf android-sdk_r${ANDROID_SDK_VERSION}-linux.tgz
 echo yes | android update sdk --filter tools,platform-tools,build-tools-21.1.1,android-19,extra-android-support,extra-android-m2repository,extra-google-m2repository --no-ui --force --no-https --all > /dev/null
-
-# create local.properties for Android build system to find SDK
-
-#echo sdk.dir=${ANDROID_HOME} > local.properties
